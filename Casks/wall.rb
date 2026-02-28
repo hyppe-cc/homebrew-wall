bad59fe1db81c618ec372a2ee4c30162174c4b6d
fermin villanueva <FerVillanuev4s@gmail.com>
2026-02-28 14:20:40 -0700

    sha

--- Casks/wall.rb
+++ Casks/wall.rb
@@ -1,6 +1,6 @@
 cask "wall" do
   version "1.0.0"
-  sha256 "SHA_OF_YOUR_DMG"
+  sha256 "e026ff6ddfbe74f4411ca83139aef591335cf6eadeb9c73ba1c3ac43fb4f40dd"
 
   url "https://github.com/hyppe-cc/wall/releases/download/v#{version}/wall.dmg"
   name "wall"
